6ac9b468b52dc0dd90c4bd0fdcf2975c54a65275
Andrei Kasak <grindrt@gmail.com>
2017-12-01 10:19:13 +0300

    Add page, babel

---
+++ 02_babel/js/script.js
@@ -0,0 +1,32 @@
+import { NewsModel, InheritanceForInheritance} from "./lib/models.js";
+
+let getUrl = (endpoint, ...sourcesAtr) => {
+  const apiKey = 'cbcde34889f4417b9dce339f88f24de8';
+  let sources = sourcesAtr.join(', ');
+  return `https://newsapi.org/v2/${endpoint}?sources=${sources}&apiKey=${apiKey}`;
+}
+let getNews = (source) => {
+  let endpoint = 'top-headlines';
+  let url = getUrl(endpoint, source);
+  fetch(url)
+  .then((resp)=>{
+  	resp.json().then(fillNews);
+  })
+  .catch(
+    (err)=>{
+      console.log(err);
+    });
+}
+
+let fillNews = (data) => {
+  let {articles} = data;
+  let newsMap = new Map();
+  articles.forEach((item, index)=>{ newsMap.set(index, new NewsModel(item.title, item.description, item.url, item.urlToImage)); });
+  let html = () => {
+    let html ='';
+    for (item of this.newsMap) { html += item.toHtml(); }
+    return html;
+  }
+
+  document.getElementById("news").innerHTML = html();
+}
