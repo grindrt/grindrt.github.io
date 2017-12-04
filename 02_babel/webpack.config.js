7c425428af8a81ad62d2d59659b9fe7506bf9c9a
Andrei Kasak <grindrt@gmail.com>
2017-12-04 23:50:22 +0300

    Add a lot of extra polyfills that didn't make IE10 happy with dataset and fetch

--- 02_babel/webpack.config.js
+++ 02_babel/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: "./js/script.js",
+     entry: ["element-dataset", "whatwg-fetch", "babel-polyfill", "./js/script.js"],
      output: {
          path: path.resolve(__dirname, 'build'),
          filename: 'app.bundle.js'
