c81c039613f0a29c585ce781288587801e6591e3
Andrei Kasak <grindrt@gmail.com>
2017-12-11 02:46:38 +0300

    Add scss/sass loaders, webpack-dev-server

--- 03_webpack/js/app.js
+++ 03_webpack/js/app.js
@@ -1,11 +1,12 @@
 import 'whatwg-fetch';
 import 'babel-polyfill';
+require("../styles/main.scss");
 import { NewsModel, InheritanceForInheritance} from "./lib/models.js";
 
 const buttons = Array.from(document.querySelectorAll('[data-news]'));
 buttons.forEach((button)=>{
   button.addEventListener('click', ()=>{
-    const source = button.dataset.source;
+    const source = button.getAttribute('data-source');
     getNews(source);
   })
 });
--- 03_webpack/webpack.config.js
+++ 03_webpack/webpack.config.js
@@ -1,8 +1,20 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "styles.css",
+    disable: process.env.NODE_ENV === "development",
+    allChunks: true
+});
 
 module.exports = {
-     entry: ["element-dataset", "whatwg-fetch", "babel-polyfill", "./js/script.js"],
+     entry: [
+       "element-dataset",
+       "whatwg-fetch",
+       "babel-polyfill",
+       "./js/app.js"
+     ],
      output: {
          path: path.resolve(__dirname, 'build'),
          filename: 'app.bundle.js'
@@ -14,10 +26,39 @@ module.exports = {
                  loader: 'babel-loader',
                  query: {
                      presets: ['es2015']
-                 }
-             }
-         ]
+                 },
+                 exclude: /(node_modules)/
+             },
+            // {
+            //     test: /\.scss$/,
+            //     loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?sourceMap')
+            // },
+            // {
+            //     test: /\.css$/,
+            //     loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            // },
+            {
+                test: /\.jpe?g$/,
+                loader: 'file-loader'
+            }
+         ],
+         rules: [{
+             test: /\.(css|scss|sass)$/,
+             use: extractSass.extract({
+               use: [{
+                   loader: "css-loader"
+                 }, {
+                   loader: "sass-loader"
+                 }],
+                 // use style-loader in development
+                 fallback: "style-loader"
+               })
+         },
+       ]
      },
+     plugins: [
+       extractSass
+    ],
      stats: {
          colors: true
      },
